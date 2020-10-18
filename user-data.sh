d3069bda7315a397b9cf92c01687ec660e99c066
zawadal <lukasz.z@zamat.pl>
2020-10-18 18:35:16 +0200

    Sample

--- user-data.sh
+++ user-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://github.com/prometheus/prometheus/releases/download/v2.22.0/prometheus-2.22.0.linux-amd64.tar.gz
-tar xvfz prometheus-*.tar.gz
-cd prometheus-*
+tar xvfz prometheus-2.22.0.linux-amd64.tar.gz
+cd prometheus-2.22.0.linux-amd64
 cp ../prometheus.yml .
 ./prometheus --config.file=prometheus.yml
