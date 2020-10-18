6f37d964e28bd811ab47d888ac744983ada9713e
zawadal <lukasz.z@zamat.pl>
2020-10-18 18:28:44 +0200

    Changing file strucutre

--- user-data.sh
+++ user-data.sh
@@ -2,6 +2,6 @@
 
 wget https://github.com/prometheus/prometheus/releases/download/v2.22.0/prometheus-2.22.0.linux-amd64.tar.gz
 tar xvfz prometheus-*.tar.gz
-cp prometheus.yml prometheus-*
 cd prometheus-*
+cp ../prometheus.yml .
 ./prometheus --config.file=prometheus.yml
