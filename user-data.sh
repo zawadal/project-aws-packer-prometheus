98000be26caf07621fedc455a84d95bf72b08e4d
zawadal <lukasz.z@zamat.pl>
2020-10-18 17:58:27 +0200

    Update

--- user-data.sh
+++ user-data.sh
@@ -2,24 +2,6 @@
 
 wget https://github.com/prometheus/prometheus/releases/download/v2.22.0/prometheus-2.22.0.linux-amd64.tar.gz
 tar xvfz prometheus-*.tar.gz
+cp prometheus.yml prometheus-*
 cd prometheus-*
-echo "global:
-  scrape_interval:     15s # By default, scrape targets every 15 seconds.
-
-  # Attach these labels to any time series or alerts when communicating with
-  # external systems (federation, remote storage, Alertmanager).
-  external_labels:
-    monitor: 'codelab-monitor'
-
-# A scrape configuration containing exactly one endpoint to scrape:
-# Here it's Prometheus itself.
-scrape_configs:
-  # The job name is added as a label `job=<job_name>` to any timeseries scraped from this config.
-  - job_name: 'prometheus'
-
-    # Override the global default and scrape targets from this job every 5 seconds.
-    scrape_interval: 5s
-
-    static_configs:
-      - targets: ['localhost:9090']" > prometheus.yml
 ./prometheus --config.file=prometheus.yml
