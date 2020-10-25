766843f5d71e9d45ed62c5afcd0031b5e25eb8a5
zawadal <lukasz.z@zamat.pl>
2020-10-25 17:42:51 +0100

    Update

--- packer/user-data.sh
+++ packer/user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mystartlocation = `pwd`
+mystartlocation=`pwd`
 cd /opt
 wget https://github.com/prometheus/prometheus/releases/download/v2.22.0/prometheus-2.22.0.linux-amd64.tar.gz
 tar xvfz prometheus-2.22.0.linux-amd64.tar.gz
